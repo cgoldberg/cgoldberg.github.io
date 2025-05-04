d5a7d518dd2e4a22b16ffced7297cb5eeb2f55c4
Corey Goldberg <1113081+cgoldberg@users.noreply.github.com>
2025-05-04 10:37:53 -0400

    Don't sort Recently Updated

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -2,13 +2,16 @@
 #
 # Check for changed posts
 
-Jekyll::Hooks.register :posts, :post_init do |post|
+# Uncomment this to have the "Recently Updated" section in the
+# right panel sorted by date/time of the last edited post
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
-
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
-  end
-
-end
+#Jekyll::Hooks.register :posts, :post_init do |post|
+#
+#  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+#
+#  if commit_num.to_i > 1
+#    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+#    post.data['last_modified_at'] = lastmod_date
+#  end
+#
+#end
